06f9fc5e30c727ee4e6fee1e26b026eb54f90171
Mikhail Demchenko <dev.echo.mike@gmail.com>
2017-10-21 18:23:37 +0400

    ...

--- main.hpp
+++ main.hpp
@@ -2,10 +2,6 @@
 #ifndef MAIN_HPP__
 #define	MAIN_HPP__ "0.0.0@main.hpp"
 #include <iostream>
-#include "Base.h"
-#include "PiDxe.h"
-#include "PiPei.h"
-#include "PiSmm.h"
-#include "Uefi.h"
+#include "stdafx.h"
 
 #endif // !MAIN_H__
---
+++ stdafx.h
@@ -0,0 +1,5 @@
+#include "Base.h"
+#include "PiDxe.h"
+#include "PiPei.h"
+#include "PiSmm.h"
+#include "Uefi.h"
