2a2d6d384c9d620a863ef12978aecdcc9f00ba08
Mikhail Demchenko <dev.echo.mike@gmail.com>
2017-10-21 17:19:40 +0400

    Initial

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <main.hpp>
+
+int main(int argc, char* argv[]) 
+{
+	int _value = 1;
+	std::cout << _value + _value << std::endl;
+	for (auto _index = 0; _index <= argc; _index++)
+		std::cout << argv[_index] << std::endl;
+	std::cin.get();
+	return 1;
+}
---
+++ main.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef MAIN_HPP__
+#define	MAIN_HPP__ "0.0.0@main.hpp"
+#include <iostream>
+#include "Base.h"
+#include "PiDxe.h"
+#include "PiPei.h"
+#include "PiSmm.h"
+#include "Uefi.h"
+
+#endif // !MAIN_H__
