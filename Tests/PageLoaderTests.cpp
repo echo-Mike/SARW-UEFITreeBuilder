f819b806581f75d9235e30c5852f7c1738305346
Mikhail Demchenko <dev.echo.mike@gmail.com>
2017-10-22 18:01:23 +0400

    Tests for Page loader updated

--- Tests/PageLoaderTests.cpp
+++ Tests/PageLoaderTests.cpp
@@ -9,11 +9,90 @@ Arguments::Option opts[] = {
 	{nullptr, nullptr, Arguments::ArgTypes::Null, 0}
 };
 
-int DefaultConstructorTest() {
+int DefaultConstructor() {
     try {
         Project::PageLoader inputFileReader(PAGE_SIZE);
     }
     catch (...) {
+        DEBUG_NEW_MESSAGE("DefaultConstructor")
+        DEBUG_END_MESSAGE
+        return 0;
+    }
+    return 1;
+}
+
+int ConstructionFromFileName(char* argv[]) {
+    try {
+        Project::PageLoader inputFileReader(argv[opts[0].data], PAGE_SIZE);
+    }
+    catch (...) {
+        DEBUG_NEW_MESSAGE("ConstructionFromFileName")
+        DEBUG_END_MESSAGE
+        return 0;
+    }
+    return 1;
+}
+
+int NextPageTest(char* argv[]) {
+    try {
+        Project::PageLoader inputFileReader(argv[opts[0].data], PAGE_SIZE);
+        if (!inputFileReader.nextPage()) return 0;
+    }
+    catch (...) {
+        DEBUG_NEW_MESSAGE("NextPageTest")
+        DEBUG_END_MESSAGE
+        return 0;
+    }
+    return 1;
+}
+
+int WholeFileReadTest(char* argv[]) {
+    try {
+        Project::PageLoader inputFileReader(argv[opts[0].data], PAGE_SIZE);
+        int _counter = 0;
+		while (inputFileReader.nextPage())
+            ++_counter;
+        DEBUG_NEW_MESSAGE("WholeFileReadTest: ")
+            DEBUG_PRINT2("\tTotal pages readed: ", ++_counter);
+            DEBUG_PRINT2("\tSize of last page: ", inputFileReader.currentPayload);
+        DEBUG_END_MESSAGE
+    }
+    catch (...) {
+        DEBUG_NEW_MESSAGE("WholeFileReadTest")
+        DEBUG_END_MESSAGE
+        return 0;
+    }
+    return 1;
+}
+
+int mergeWithNextPageTest(char* argv[]) {
+    try {
+        Project::PageLoader inputFileReader(argv[opts[0].data], PAGE_SIZE);
+        for (int i = 0; i < 10; ++i)
+            inputFileReader.nextPage();
+        auto _buffer = inputFileReader.mergeWithNextPage();
+        return _buffer.first == PAGE_SIZE * 2;
+    }
+    catch (...) {
+        DEBUG_NEW_MESSAGE("mergeWithNextPageTest")
+        DEBUG_END_MESSAGE
+        return 0;
+    }
+    return 1;
+}
+
+int mergeWithNextPageBuferisedTest(char* argv[]) {
+    try {
+        Project::PageLoader inputFileReader(argv[opts[0].data], PAGE_SIZE);
+        inputFileReader.nextPage();
+        auto _buffer = inputFileReader.mergeWithNextPage();
+        for (int i = 0; i < 10; ++i)
+            _buffer = inputFileReader.mergeWithNextPage(std::move(_buffer)); 
+        return _buffer.first == PAGE_SIZE * 12;
+    }
+    catch (...) {
+        DEBUG_NEW_MESSAGE("mergeWithNextPageTest")
+        DEBUG_END_MESSAGE
         return 0;
     }
     return 1;
@@ -23,34 +102,22 @@ int main(int argc, char* argv[])
 {
     Arguments::parseArgs(argc, argv, opts);
     
-    int _testCounter;
-
-    Project::PageLoader inputFileReader(PAGE_SIZE);
-
     if (opts[0].data == -1)
 		DEBUG_NEW_MESSAGE("MAIN::INPUT_FILE_OPTION")
 			DEBUG_PRINT1("\tCan't open input file: No file path provided.");
 		DEBUG_END_MESSAGE_AND_EXIT(-1)
-	else
-		inputFileReader.open(argv[opts[0].data], std::fstream::in | std::fstream::binary);
 
-    if (inputFileReader) {
-		std::cout << "Success\n";
-		std::cout << "\tFile path: " << argv[opts[0].data] << std::endl;
-		
-		if (inputFileReader.nextPage())
-			std::cout << "Page read success" << std::endl;
-		
-		int _counter = 0;
-		while (inputFileReader.nextPage())
-			++_counter;
-
-		std::cout << "Total pages: " << _counter + 1 << "\n";
-		std::cout << "Last page size: " << inputFileReader.currentPayload << std::endl;
-
-		inputFileReader.close();
-	}
-
+    int _passedTests = 0;
+    _passedTests += DefaultConstructor();
+    _passedTests += ConstructionFromFileName(argv);
+    _passedTests += NextPageTest(argv);
+    _passedTests += WholeFileReadTest(argv);
+    _passedTests += mergeWithNextPageTest(argv);
+    _passedTests += mergeWithNextPageBuferisedTest(argv);
+    
+    DEBUG_NEW_MESSAGE("Test statistics: ")
+        DEBUG_PRINT3("\tTests passed ", _passedTests, " out of 6");
+    DEBUG_END_MESSAGE
 	std::cin.get();
 	return 0;
 }
