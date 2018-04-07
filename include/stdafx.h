b323eabdc8016f795803872f041956a6a808f0ba
Mikhail Demchenko <dev.echo.mike@gmail.com>
2018-04-07 12:38:54 +0400

    VS project and solution added.

--- include/stdafx.h
+++
@@ -1,9 +0,0 @@
-extern "C" {
-    #include "Base.h"
-    #include "PiDxe.h"
-    #include "PiPei.h"
-    #include "PiSmm.h"
-    #include "Uefi.h"
-    #include <Pi/PiFirmwareVolume.h>
-    #include <Pi/PiFirmwareFile.h>
-}
